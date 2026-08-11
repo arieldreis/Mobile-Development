cafebacd271503391e73c43f075930bf46833739
Ariel Marinho <arieldreis@gmail.com>
2026-08-11 18:12:57 -0300

    Enhance Home screen with additional navigation buttons
    Added new buttons for 'Entrar' and 'Estudo do Flex' navigation.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,39 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native"
 export default function Home(){
     const nav = useNavigation();
     return(
         <View>
             <Text>Home</Text>
-            <Button title="About Us" color="#ff0000" 
+            <Button 
+            title="About Us" 
+            color="#ff0000" 
             onPress={() => nav.navigate("Sobre")}
             />
+
+            <Button 
+            title="Entrar" 
+            onPress={() => nav.navigate('Login')}
+            />
+            <Button 
+            title="Estudo do Flex" 
+            onPress={() => nav.navigate('Flex')} 
+            />
+
             {/* SE UMA FUNÇÃO ARROW TIVER SÓ UMA LINHA DE CÓDIGO NÃO PRECISA DE CHAVES*/}
+            <Text></Text>
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    contenador: {
+        flex: 1,
+        gap: 15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    txt: {
+        fontSize: 30,
+    },
+});
